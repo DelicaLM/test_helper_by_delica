9bb3ac88071ef6fa62c9ff3c46fc7b5301ca00c5
delicalm <stormindustries22@outlook.com>
2026-03-08 21:02:33 -0500

    Added sphinx documentation support.

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
-from src.test_helper_funcs import *#create_unittest_obj, run_func_tests, test_bool_func
+from test_helper_funcs import *#create_unittest_obj, run_func_tests, test_bool_func
 
-from src.IOPair import IOPair
+from IOPair import IOPair
 
 
