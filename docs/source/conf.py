a7fe055dbaa2cebaca71d310f7398b7e54c214a9
delicalm <stormindustries22@outlook.com>
2026-06-27 23:43:11 -0500

    Added sphinx documentation support.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../src'))
 
 project = 'test-helper-by-delica'
 copyright = '2026, Delica Leboe-McGowan'
