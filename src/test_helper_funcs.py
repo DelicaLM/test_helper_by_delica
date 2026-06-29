c1c927e38035a0c5c7d4e8c7cf360215d475d17f
delicalm <stormindustries22@outlook.com>
2026-06-29 00:40:33 -0500

    Added sphinx documentation support.

--- src/test_helper_funcs.py
+++ src/test_helper_funcs.py
@@ -28,7 +28,7 @@ test_bool_func(test_func, true_inputs=None, false_inputs=None, error_if_false=Fa
 
 # Import the IOPair class to more easily create and pass input-output pairs for tests
 #from src.IOPair import IOPair
-import IOPair
+import src.IOPair as IOPair
 
 # Import the time library to measure test runtimes.
 import time
