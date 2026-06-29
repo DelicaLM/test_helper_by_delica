f8d100201abe708552cfa812e63dcc692869cd24
delicalm <stormindustries22@outlook.com>
2026-06-29 01:02:38 -0500

    Added sphinx documentation support.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,8 +25,9 @@ extensions = ['autodoc2',
 autodoc2_docstring_parser_regexes = [
     (r".*", "rst"),
 ]
+
 autodoc_type_aliases = {
-    'IOPair': 'src.IOPair',
+    'IOPair': 'src.IOPair'
 }
 primary_domain = 'py'
 
