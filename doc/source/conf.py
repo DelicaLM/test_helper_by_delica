af4a022ba178293925be7732502c3e2cf48f5fa0
delicalm <stormindustries22@outlook.com>
2026-07-01 22:59:25 -0500

    Added sphinx documentation support.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -49,7 +49,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'basic'
 #html_static_path = ['_static']
 # html_sidebars = {
 #     '**': [
