7cfbe020805e1b78c7e8743a0e3148ce4bfdb014
delicalm <stormindustries22@outlook.com>
2026-07-03 19:43:49 -0500

    Added sphinx documentation support.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -59,7 +59,7 @@ exclude_patterns = []
 
 html_theme = 'classic'
 html_theme_options = {
-    "collapsiblesidebar": "true",
+    "sidebarwidth": "350",
 }
 # html_theme_options = {
 #     'sidebarwidth': '350px',
