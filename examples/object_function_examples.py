647e46095271928c50a443adce3eb01155a28d6d
delicalm <stormindustries22@outlook.com>
2026-07-16 20:07:15 -0500

    Added sphinx documentation support.

--- examples/object_function_examples.py
+++ examples/object_function_examples.py
@@ -3,9 +3,21 @@ Script demonstrating how we can use the test helper package to validate object-o
 if you do not include any of the optional runtime flags described below) executing this script will run all of the
 built-in demos for object-oriented tests.
 
+Parameters
+----------
+---run_empty_class_demo : bool, default False
+    Boolean flag for whether the empty class example should be run.
+---run_one_int_attr_demo : bool, default False
+    Boolean flag for whether the integer attribute class example should be run.
+---run_no_attr_one_method_demo : bool, default False
+    Boolean flag for whether the class with no attributes and one method example should be run.
+---run_one_attr_one_method_demo : bool, default False
+    Boolean flag for whether the class with one attribute and one method example should be run.
+---run_two_attr_two_method_demo : bool, default False
+    Boolean flag for whether the class with two attributes and two methods example should be run.
+---run_list_attr_demo : bool, default False
+    Boolean flag for whether the list attribute class example should be run.
 """
-
-
 
 
 
