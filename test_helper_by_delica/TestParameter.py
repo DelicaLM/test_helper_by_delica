fa1d832d6e285a2656c7c36c7fc2e42b52fa43e0
delicalm <stormindustries22@outlook.com>
2026-08-12 11:19:30 -0500

    Added sphinx documentation support.

--- test_helper_by_delica/TestParameter.py
+++ test_helper_by_delica/TestParameter.py
@@ -4,4 +4,8 @@ class TestParameter:
     def __init__(self, par_name, par_type):
         self.par_name = par_name
         self.par_type = par_type
+        self.default_val = None
+        self.min_val = None
+        self.max_val = None
+        self.other_illegal_values = []
 
